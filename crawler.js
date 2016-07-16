4265f75f5325d77354c5a0f4f424932d6f9582d1
Chen Yiwen <lumpychen@outlook.com>
2016-07-16 15:21:24 +0800

    init: project initial

---
+++ crawler.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+  // superagent = require('superagent'),
+  https = require('https'),
+  http = require('http'),
+  cheerio = require('cheerio');
+
+http.createServer((req, sres)=>{
+
+  let items = [];
+
+  https.get('https://cnodejs.org/',(res)=>{
+
+    res.on('data', (chunk) => {
+
+      let $ = cheerio.load(chunk);
+
+      $('#topic_list .topic_title').each((idx, element)=>{
+
+        var $element = $(element);
+
+        items.push({
+          title: $element.attr('title'),
+          href: $element.attr('href')
+        });
+
+      });
+
+    });
+
+    res.on('end',()=>{
+
+      sres.end(JSON.stringify(items))
+
+    })
+
+  })
+
+}).listen(3000, (err) => {
+    console.log('Server listening on port 3000...');
+})
